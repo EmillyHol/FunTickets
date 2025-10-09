d67514100117926752141910e92492cf8894f0fa
emill <emill@EMILLYH>
2025-10-09 14:04:10 -0300

    Image sprint

--- FunTickets/Models/Activite.cs
+++ FunTickets/Models/Activite.cs
@@ -1,9 +1,12 @@
-﻿using System.Net.Sockets;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
 
 namespace FunTickets.Models
 {
     public class Activite
     {
+
         public int ActiviteId { get; set; }
         public string ActiviteName { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
@@ -22,6 +25,14 @@ namespace FunTickets.Models
         public string Owner { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string? ImageFilename { get; set; }
+
+        // For storing the photograph as a byte array
+        [NotMapped]
+        [Display(Name = "Photograph")]
+        public IFormFile? FormFile { get; set; }//nullable
+      
     }
 
 }
